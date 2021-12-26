927890c5b8155be079590b21ae8d17224a23d888
klumw <klumw0430@gmail.com>
2021-12-26 13:53:11 +0900

    assignment(170)

---
+++ 170.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+void oa(int v) {
+  puts("@@@@@@@@@@");
+}
+
+int main()
+{
+  puts("first");
+  oa(0);
+  puts("second");
+  oa(0);
+  puts("third");
+  oa(0);
+
+  return 0;
+}
