831f841ab2e669767c31cbac097834bbe0af1d92
준규 <vpark45@gmail.com>
2022-01-29 01:15:07 +0900

    Accepted

---
+++ nattybear/language-coder/function3/592.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int sum_of_square(int n)
+{
+  if (n < 10)
+    return n;
+  int remainder = n % 10;
+  int quotient = n / 10;
+  int square = remainder * remainder;
+  return square + sum_of_square(quotient);
+}
+
+int main()
+{
+  int n;
+  scanf("%i", &n);
+  printf("%i\n", sum_of_square(n));
+  return 0;
+}
